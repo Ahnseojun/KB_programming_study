3a95bc493f12c11105a97fb0cc5b22a27538d85a
Ahnseojun <70713842+Ahnseojun@users.noreply.github.com>
2022-08-10 17:33:18 +0900

    java_0810

---
+++ java0810/java_Q.java
@@ -0,0 +1,26 @@
+class Student {
+	//변수 선언
+	private String name;
+	private int number;
+	private String major;
+	
+	//함수 선언
+	public String getName() {
+		return this.name;
+		}
+	public int getNumber() {
+		return this.number;
+	}
+	public String getMajor() {
+		return this.major;
+	}
+	
+	//생성자 함수
+	
+}
+
+public class java_Q {
+	public static void main(String args[]) {
+		
+	}
+}
