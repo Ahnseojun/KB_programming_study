c4ff4f5b2203aae6ee99a34e7c7ab103555f4848
Ahnseojun <orbitahj@gmail.com>
2022-09-28 10:42:24 +0900

    Update 01_student_crud.js

--- JavaScript_Training/0928/01_student_crud.js
+++ JavaScript_Training/0928/01_student_crud.js
@@ -1,18 +1,19 @@
-function Student(name, age, number) {
+function Student(name, age, number)
+{
     this.name = name
     this.age = age
     this.number = number
 }
-
+//()=>{} 이건 this가 안 된다.
 Student.prototype.regist = function() {
-    alert(this.number + ' ' + this.name + ' 학생 등록 완료')
+    alert(this.number+' '+this.name+'학생 등록 완료')
 }
 
 Student.prototype.toString = function() {
-    return this.name + ' ' + this.number + ' ' + this.age
+    return this.name+' '+this.number+' '+this.age
 }
-
 window.onload = () => {
+    
     let my_name = document.querySelector('#name')
     let my_age = document.querySelector('#age')
     let my_num = document.querySelector('#num')
@@ -22,48 +23,59 @@ window.onload = () => {
     let my_update = document.getElementById('update')
     let my_delete = document.getElementById('delete')
 
-    let my_console_log = document.getElementById('console_log')
+    let my_console_log = 
+        document.getElementById('console_log')
+
 
     let my_students = []
 
     my_create.onclick = function() {
         for (const iterator of my_students) {
-            if(iterator.number == my_num.value) {
+            if(iterator.number == my_num.value)
+            {
                 alert('중복 학생 존재')
                 return
-            } 
+            }
         }
-        let tempStudent = new Student(my_name.value, my_age.value, my_num.value)
+        let tempStudent = new Student(my_name.value,
+            my_age.value,my_num.value)
         my_students.push(tempStudent)
-        tempStudent.regist() // 해당 학생의 정보를 띄움
-        // 내가 정의한 toString
+        tempStudent.regist() //해당 학생의 정보를 띄움
+
+        //내가 정의한 toString 출력
         console.log(my_students.toString())
-        // 
-        console.log(my_students)
+        //배열 자체를 출력
+        console.log(my_students) 
     }
 
-    function findStudent() {
-        // findIndex : 객체형태의 배열에서 특정 조건에 맞는 객체가 몇 번에 있는지 찾기
-        // ex) my_num에 적힌 값이랑 number값이 같은 거 찾기
+    function findStudent()
+    {
+        //findIndex : 객체형태의 배열에서
+        //특정 조건에 맞는 객체가 몇 번에 있는지 찾기
+        //ex) my_num에 적힌 값이랑 number값이 같은 거 찾기
 
-        // indexOf : 숫자나 문자열 형태의 배열에서 찾고자 하는 값의 위치(=인덱스) 찾음
-      const idx = my_students.findIndex(
-        function(item) {
-            return item.number == my_num.value
+        //indexOf : 숫자나 문자열 형태의 배열에서
+        //찾고자 하는 값의 위치(=인덱스) 찾음
+
+        //이 둘의 공통점은 찾고자 하는 값이 없을 때 -1을 반환
+        const idx = my_students.findIndex (
+
+            function(item) {
+                return item.number == my_num.value
+            }
+        )
+        if(idx!=-1)
+        {
+            alert(`${my_students[idx].number}번 학생은 ${my_students[idx].name}`)
         }
-      )
-      if(idx != -1) {
-        alert(`${my_students[idx].number}번 학생은 ${my_students[idx].name}`)
-      }
     }
-    my_read.onclick = findStudent
 
     my_read.onclick = findStudent
 
     my_update.addEventListener('click', () => {
         const idx = my_students.findIndex(
-        function(item)   {
-                    return item.number == my_num.value
+            function(item) {
+                return item.number==my_num.value
             }
         )
         if(idx != -1) {
@@ -71,5 +83,25 @@ window.onload = () => {
             my_students[idx].age = my_age.value
         }
     })
+
+    my_delete.addEventListener('click', function(){
+        for(let i = 0; i<my_students.length;i++) {
+            if(my_num.value==my_students[i].number)
+            {
+                my_students.splice(i,1) //i번째꺼 1개 제거
+                alert('학생 삭제 완료')
+                return
+            }
+        }
+        
+        alert('해당 번호의 학생 없음')
+    })
+
+    let allSelect = function() {
+        for (const iterator of my_students) {
+            console.log(iterator.toString())
+        }
+    }
+    my_console_log.addEventListener('click', allSelect)
 
 }
