e449d1dc5a52e3072fab637ebb3b7d5cedfd4fb3
Ahnseojun <70713842+Ahnseojun@users.noreply.github.com>
2022-11-17 09:14:52 +0900

    Add files via upload

---
+++ C_Sharp/HelloMyCSharp03/HelloMyCSharp03/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMyCSharp03
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_mbox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1_input.Text+"를 입력하셨습니다");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SubForm s = new SubForm(textBox1_input.Text);
+            s.Show();
+        }
+
+        private void button_show_Click(object sender, EventArgs e)
+        {
+            //기본 생성자를 이용하여 만듦
+            SubForm s = new SubForm();
+            s.Show();
+        }
+
+        private void circularButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ C_Sharp/HelloMyCSharp03/HelloMyCSharp03_02/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMyCSharp03_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text =="1234")
+            {
+                MessageBox.Show("관리자님 환영합니다^^");
+            }
+            if(textBox1.Text == null || textBox2.Text == null)
+            {
+                MessageBox.Show("아이디 / 비밀번호가 비었습니다");
+                return;
+            }
+          
+        }
+
+
+    }
+}
