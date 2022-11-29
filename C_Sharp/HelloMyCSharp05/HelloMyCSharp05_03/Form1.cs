97d36b31a709107fd3d59f78125fd3f72a264ca2
Ahnseojun <70713842+Ahnseojun@users.noreply.github.com>
2022-11-29 21:05:18 +0900

    CSharp05

---
+++ C_Sharp/HelloMyCSharp05/HelloMyCSharp05_03/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMyCSharp05_03
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int[] array = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+            Random random = new Random();
+            int num = random.Next(1, 46);
+
+                if (array.Contains(num) != true)
+                {
+                    array[i] = num;
+                }
+                else
+                {
+                    i--;
+                }
+               
+            }
+
+            Array.Sort(array, 0, 6);
+            label1.Text = String.Join(", ", array);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+    }
+}
