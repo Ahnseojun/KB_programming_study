2110fc1ebf447a341956037163a5d13775e5713e
Ahnseojun <70713842+Ahnseojun@users.noreply.github.com>
2022-11-29 21:11:14 +0900

    CSharp07

---
+++ C_Sharp/HelloMyCSharp07/HelloMyCSharp07/MyChild.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMyCSharp07
+{
+    internal class MyChild : MyParent
+    {
+        public MyChild()
+        {
+            MyParent.money--;
+        }
+
+        public void study()
+        {
+            Console.WriteLine("공부하쟈~");
+        }
+
+        public void playgame()
+        {
+            Console.WriteLine("롤 드가자");
+        }
+
+        public void buyBitCoin(int money)
+        {
+            MyParent.money -= money;
+            
+            Console.WriteLine("남은 money : " + money);
+        }
+ 
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ C_Sharp/HelloMyCSharp07/HelloMyCSharp07/MyParent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMyCSharp07
+{
+    public class MyParent
+    {
+        string name;
+        int age;
+
+        public static int money;
+        static void openDoor()
+        {
+            Console.WriteLine("가정의 도어락");
+        }
+
+        void eat()
+        {
+
+        }
+
+        void sleep()
+        {
+
+        }
+
+    }
+}
